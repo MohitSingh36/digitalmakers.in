5e471370d51fa39ef21c871ade69632dd8a699de
Mohit Singh <136870726+MohitSingh36@users.noreply.github.com>
2024-09-14 20:53:41 +0530

    GTag added

--- app/Views/components/head.php
+++ app/Views/components/head.php
@@ -57,7 +57,15 @@
 
 <!-- Google Tag Manager -->
 <?php if(!getenv('SITE_TYPE') == 'local'): ?>
- 
+  <!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-G0M5HS3SNX"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-G0M5HS3SNX');
+  </script>
 <?php endif; ?>
 
 <?=$this->include("components/bootstrap-css-min") ?>
