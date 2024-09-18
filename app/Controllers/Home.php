9103514c8f8df3e27e075ba6b54688c8f1b06baf
Mohit Singh <136870726+MohitSingh36@users.noreply.github.com>
2024-09-18 13:54:16 +0530

    Update Home.php

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -8,8 +8,8 @@ class Home extends BaseController
     {
         $data = [
             "head" => [
-                "title" => "Your Digital Transformation Partner | Digital Makers",
-                "description" => "Digital Makers is a Leading Web Development, SEO, Editing, Designing and Digital Services agency based in India, committed to helping businesses grow online.",
+                "title" => "Digital Makers - Your Digital Transformation Partner",
+                "description" => "Digital Makers is a leading web development, SEO, editing, designing and digital services agency based in India, committed to helping businesses grow online.",
                 "keywords" => "digital makers, digital makers india, .in, digitalmakers.in, web development, web designing, seo, wikipedia page creation, video editing, voice editing, social media management, digital marketing, branding, digital transformation, online presence"
             ],
         ];        
